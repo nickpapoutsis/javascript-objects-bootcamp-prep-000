b41aab28f9b21393bd10118e4149f766ba773394
Nick Papoutsis <izybit@gmail.com>
2017-09-30 18:26:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(obj, artist, song){
   Object.assign(obj, {[artist]: song})
 return obj;
 }
+
+function removeFromPlaylist(obj, artist){
+  delete obj.artist;
+  return obj;
+}
